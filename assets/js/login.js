6690b6d46d54b5c289670d714b84a4504a707b6d
huamaiyu <1219401250@qq.com>
2022-01-25 14:37:00 +0800

    更新

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,38 +1,38 @@
-$(function() {
+$(function () {
     //点击"去注册账号"的链接
-    $('#link_reg').on('click', function() {
-            $('.login-box').hide()
-            $('.reg-box').show()
-        })
-        //点击"去登录"的链接
-    $('#link_login').on('click', function() {
-            $('.login-box').show()
-            $('.reg-box').hide()
-        })
-        //从layui中获取form
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    //点击"去登录"的链接
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+    //从layui中获取form
     var form = layui.form
     var layer = layui.layer
-        //通过form.verify （）函数自定义校验规则
+    //通过form.verify （）函数自定义校验规则
     form.verify({
-            //自义定一个叫pwd的校验规则
-            pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-            repwd: function(vaule) {
-                var pwd = $('.reg-box [name=password]').val()
-                if (pwd !== vaule) {
-                    return '两次输入的密码不一致'
-                }
+        //自义定一个叫pwd的校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (vaule) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== vaule) {
+                return '两次输入的密码不一致'
             }
-        })
-        //监听注册表单的提交事件
-    $('#form_reg').on('submit', function(e) {
+        }
+    })
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
         //阻止默认提交行为
         e.preventDefault()
-            //发起ajax的提交请求
+        //发起ajax的提交请求
         $.post('/api/reguser', {
-                username: $('#form_reg [name=username]').val(),
-                password: $('#form_reg [name=password').val()
-            },
-            function(res) {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password').val()
+        },
+            function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message);
                 }
@@ -42,7 +42,7 @@ $(function() {
     })
 
     //监听登录表单的提交事件
-    $('#form_login').on('submit', function(e) {
+    $('#form_login').on('submit', function (e) {
         //阻止默认提交行为
         e.preventDefault()
         $.ajax({
@@ -50,14 +50,14 @@ $(function() {
             method: 'POST',
             //快速获取表单中的数据
             data: $(this).serialize(),
-            success: function(res) {
+            success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
                 layer.msg('登录成功')
                 localStorage.setItem('token', res.token)
-                    //跳转到后台主页
-                location.href = '/index.html'
+                //跳转到后台主页
+                location.href = '/大事件项目/index.html'
             }
         })
 
